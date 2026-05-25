2347f429612b04a8eceb36b8bf06b89a96059ae5
Daniel <24160684@itoaxaca.edu.mx>
2026-05-25 10:57:33 -0600

    actaulizo

---
+++ conexion.php
@@ -0,0 +1,13 @@
+<?php
+// Configuración de la base de datos
+$host = "localhost";
+$user = "dev_user";
+$password = "Devuser*2026"; // <-- Pon tu contraseña real de MySQL aquí
+$database = "italikacuatro";          // <-- Pon el nombre real de tu BD aquí
+
+$conexion = mysqli_connect($host, $user, $password, $database);
+
+if (!$conexion) {
+    die("Error crítico de conexión: " . mysqli_connect_error());
+}
+?>
