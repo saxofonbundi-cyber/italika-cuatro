0383ce4014b20d0ccf06771e150b434e2794abdb
Daniel <24160684@itoaxaca.edu.mx>
2026-05-25 21:54:57 -0600

    nuevo

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 // Configuración de la base de datos
 $host = "localhost";
 $user = "dev_user";
-$password = "Devuser*2026"; // <-- Pon tu contraseña real de MySQL aquí
+$password = "Devuser¿2026"; // <-- Pon tu contraseña real de MySQL aquí
 $database = "italikacuatro";          // <-- Pon el nombre real de tu BD aquí
 
 $conexion = mysqli_connect($host, $user, $password, $database);
